ed3177ad559d240a91302a69909289af46ce407c
youclamp <quincy_g@hotmail.com>
2014-09-12 09:28:55 -0400

    logging

--- app/src/main/java/example/app/MainActivity.java
+++ app/src/main/java/example/app/MainActivity.java
@@ -1,8 +1,19 @@
 package example.app;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
 // commented change
+    static final String LOG_TAG = "minubia";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e(LOG_TAG, "jst testing log features");
+    }
 }
